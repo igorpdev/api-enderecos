e68fbab667bb778dc2608f36d404920a8ad7a7ba
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-06-07 13:24:27 -0300

    update ExceptionHandler

--- src/main/java/org/igorpdev/apienderecos/handler/RestExceptionHandler.java
+++ src/main/java/org/igorpdev/apienderecos/handler/RestExceptionHandler.java
@@ -1,18 +1,40 @@
 package org.igorpdev.apienderecos.handler;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import org.igorpdev.apienderecos.error.ErrorDetails;
 import org.igorpdev.apienderecos.error.ResourceNotFoundDetails;
 import org.igorpdev.apienderecos.error.ResourceNotFoundException;
 import org.igorpdev.apienderecos.error.UserExistsDetails;
 import org.igorpdev.apienderecos.error.UserExistsException;
+import org.igorpdev.apienderecos.error.ValidationErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> defaultErrorException(Exception exception) {
+        ErrorDetails eDetails = ErrorDetails.Builder
+            .newBuilder()
+            .timestamp(new Date().getTime())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .title("Erro")
+            .detail(exception.getMessage())
+            .developerMessage(exception.getClass().getName())
+            .build();
+        return new ResponseEntity<> (eDetails, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handlerResourceNotFoundExeception(ResourceNotFoundException rnfException) {
         ResourceNotFoundDetails rnfDetails = ResourceNotFoundDetails.Builder
@@ -21,7 +43,7 @@ public class RestExceptionHandler {
             .status(HttpStatus.NOT_FOUND.value())
             .title("Resource not found")
             .detail(rnfException.getMessage())
-            .developerMessage("Usuário não encontrado.")
+            .developerMessage(rnfException.getClass().getName())
             .build();
         return new ResponseEntity<> (rnfDetails, HttpStatus.NOT_FOUND);
     }
@@ -34,8 +56,41 @@ public class RestExceptionHandler {
             .status(HttpStatus.BAD_REQUEST.value())
             .title("Usuário existente")
             .detail(userExistsException.getMessage())
-            .developerMessage("Email e/ou CPF já foram cadastrados.")
+            .developerMessage(userExistsException.getClass().getName())
             .build();
         return new ResponseEntity<> (ueDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException maNotValidException) {
+        
+        List<FieldError> fieldErrors = maNotValidException.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        ValidationErrorDetails veDetails = ValidationErrorDetails.Builder
+            .newBuilder()
+            .timestamp(new Date().getTime())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .title("Campos inválidos")
+            .detail(maNotValidException.getMessage())
+            .developerMessage(maNotValidException.getClass().getName())
+            .field(fields)
+            .fieldMessage(fieldMessages)
+            .build();
+        return new ResponseEntity<> (veDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<?> handlerInvalidFormatException(InvalidFormatException invalidFormatException) {
+        ErrorDetails eDetails = ErrorDetails.Builder
+            .newBuilder()
+            .timestamp(new Date().getTime())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .title("Campos inválidos")
+            .detail(invalidFormatException.getMessage())
+            .developerMessage(invalidFormatException.getClass().getName())
+            .build();
+        return new ResponseEntity<> (eDetails, HttpStatus.BAD_REQUEST);
+    } 
 }
